8aaeb1f235ff44deebb069fafaf2768223f5d64b
amdika <dikasvieln@gmail.com>
2023-10-16 03:03:29 +0700

    update initial algorithm for codingbats

--- AlgoStruct.cpp
+++ AlgoStruct.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <bits/stdc++.h>
-#include <iterator>
+//#include <iterator>
 #include <sstream>
 #include "Fruits.h"
 #include "Solutions.h"
@@ -33,36 +33,57 @@ public:
 
 int main()
 {
-    Fruits s;
-    Fruits a;
-    Fruits d;
-
-    Solutions codeTen;
+    
     Solutions changes;
     Solutions dtd;
     
     string get;
-    int teps;
-    int tips;
-    int data;
-    int inputA;
-    int inputB;
-    int getA;
-    int getB;
     int *chA;
 
     int size;
 
-    //var data = new variable();
-
-    /*auto random_numb = launch::xorshift(4);
-
-    for (auto u : random_numb) {
-        cout << u << '\n';
-    }*/
-
+ 
+    int dt;
     
     cout << "Hello everyone!" << endl;
+
+    do
+     {
+        
+        int odd;
+        int even;
+
+        cout << "list of algorithm practice" << endl;
+        cout << "1. Odd number" << endl;
+        cout << "2. Abs number" << endl;
+        cout << "5. exit programs" << endl;
+
+        cout << "input your choose (1..5): ";
+        cin >> dt;
+
+        switch (dt)
+        {
+        case 1:
+            cout << "testing odd number" << endl;
+            cout << "input for case odd  : ";
+            cin >> odd;
+  
+            cout << "the answer is : " << changes.oddNumb(odd) << endl;
+            break;
+        case 2:
+            cout << "testing abs10" << endl;
+            cout << "input A for case abs 10 : ";
+            cin >> odd;
+            cout << "input B for case abs 10 : ";
+            cin >> even;
+
+            cout << "the answer is : " << changes.abs10(odd, even) << endl;
+            break;
+        default:
+            cout << "close the program by ctrl + c or type: exit";
+            break;
+        }
+    } while (dt != 5);
     
    /* cout << "Count your fruits : " << endl;
     
@@ -95,15 +116,8 @@ int main()
 
     cout << "the answer is : " << changes.abs10(getA, getB) << endl;*/
 
-    int odd;
 
-    cout << "input for case odd  : ";
-    cin >> odd;
-    string result = to_string(dtd.oddNumb(odd));
-
-  
-
-    cout << "the answer is : " << odd << endl;
+    
 
 }
 
